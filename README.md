fc66fe6852f95e83a9d439691f67fa8bd04f59ca
JUkhan <jasim.uddin.khan@gmail.com>
2022-05-20 22:09:18 +0600

    v0.0.2: added readme content and modify dragging and resizing logic

--- README.md
+++ README.md
@@ -1,2 +1,70 @@
 # resize-figure-ng-dotnet
 resize figure with angular and asp.net core api
+
+### `figureApi` for reading and updating a json file. Here is swagger link of this Api: https://localhost:7017/swagger/index.html. This api has a single `FigureController`.
+
+### FigureController
+```cs
+public class FigureController : ControllerBase
+{
+
+  private readonly ILogger<FigureController> _logger;
+  private readonly IFigureService _figureService;
+
+  public FigureController(ILogger<FigureController> logger, IFigureService figureService)
+  {
+    _logger = logger;
+    _figureService = figureService;
+  }
+
+  [HttpGet(Name = "GetFigures")]
+  public Task<IEnumerable<Figure>> Get() => _figureService.GetFigures();
+
+  [HttpPut(Name = "UpdateFigure")]
+  public Task<Figure> Put([FromBody] Figure figure) => _figureService.UpdateFigure(figure);
+
+}
+
+```
+### `angularApp` consumes `figureApi` for initially loading figure data and later updating figures' position and size. Here is D3 for rendering figures inside `canvas.component`.
+
+### api.service
+```ts
+export class ApiService {
+  readonly baseUrl = 'https://localhost:7017/figure/';
+  constructor(private httpClient: HttpClient) {}
+
+  getFigures(): Observable<Figure[]> {
+    return this.httpClient.get(this.baseUrl) as Observable<Figure[]>;
+  }
+  updateFigure(figure: Figure): Observable<Figure> {
+    return this.httpClient.put(this.baseUrl, figure) as Observable<Figure>;
+  }
+}
+```
+There is another `app.service` for app state management that also have debouncing update method. That notifies `canvas.component` whenever necessary state changes.
+
+### app.service
+```ts
+export class AppService extends StateController<AppState> {
+  constructor(private api: ApiService) {
+    super({
+      data: [],
+    });
+  }
+  updateFigure = this.effect<Figure>((rect$) =>
+    rect$.pipe(
+      debounceTime(200),
+      switchMap((figure) => this.api.updateFigure(figure)),
+      tap((figure) => console.log('updated-figure:', figure)),
+      map((figure) => ({
+        data: this.state.data.map((r) => (r.id === figure.id ? figure : r)),
+      }))
+    )
+  );
+  
+  getFigures() {
+    this.api.getFigures().subscribe((figures) => this.emit({ data: figures }));
+  }
+}
+```
--- angularApp/src/app/canvas/canvas.component.ts
+++ angularApp/src/app/canvas/canvas.component.ts
@@ -21,7 +21,8 @@ export class CanvasComponent implements OnInit, OnDestroy {
   ngOnInit(): void {
     const svg = select(this.canvas.nativeElement)
       .attr('width', this.width)
-      .attr('height', this.height);
+      .attr('height', this.height)
+      .attr('viewBox', `0 0 ${this.width} ${this.height}`);
     this.service.getFigures();
     const [drag, resize] = this.dragAndResize(svg, this.service);
     this.subs = this.service
@@ -37,22 +38,52 @@ export class CanvasComponent implements OnInit, OnDestroy {
     function distance(p1: any, p2: any) {
       return Math.pow(Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2), 0.5);
     }
+    let cp = [0, 0],
+      g!: Selection<SVGGElement, unknown, null, undefined>,
+      W = this.width,
+      H = this.height,
+      dragStart = false;
     const dragGroup = drag()
       .on('drag', function (d) {
-        let dx = d.sourceEvent.offsetX - 50,
-          dy = d.sourceEvent.offsetY - 30;
-        const g = svg.select(`g#id${d.subject.id}`);
+        if (!dragStart) return;
+        let x = d.sourceEvent.offsetX,
+          y = d.sourceEvent.offsetY;
+        cp[0] = cp[0] <= x ? x - cp[0] : (cp[0] - x) * -1;
+        cp[1] = cp[1] <= y ? y - cp[1] : (cp[1] - y) * -1;
+        cp[2] += cp[0];
+        cp[3] += cp[1];
+        if (
+          cp[2] <= 0 ||
+          cp[3] <= 16 ||
+          cp[2] + cp[4] >= W ||
+          cp[3] + cp[5] >= H
+        ) {
+          dragStart = false;
+          return;
+        }
+        g.attr('transform', `translate(${cp[2]},${cp[3]} )`)
+          .attr('x', (d) => cp[2])
+          .attr('y', (d) => cp[3]);
+        cp[0] = x;
+        cp[1] = y;
 
-        g.attr('transform', `translate(${dx},${dy} )`)
-          .attr('x', (d) => dx)
-          .attr('y', (d) => dy);
-        service.updateFigure({ ...d.subject, x: dx, y: dy });
+        service.updateFigure({ ...d.subject, x: cp[2], y: cp[3] });
       })
       .on('end', function (d) {
         select(this).attr('cursor', 'default');
+        dragStart = false;
+        g.attr('z-index', '1');
       })
       .on('start', function (d) {
+        cp = [d.sourceEvent.offsetX, d.sourceEvent.offsetY];
+        g = svg.select(`g#id${d.subject.id}`);
+        cp.push(Number(g.attr('x')));
+        cp.push(Number(g.attr('y')));
+        cp.push(Number(g.select('.rect1').attr('width')));
+        cp.push(Number(g.select('.rect1').attr('height')));
         select(this).attr('cursor', 'move');
+        dragStart = true;
+        g.attr('z-index', '101');
       });
 
     const resize = drag().on('drag', function (d) {
@@ -77,16 +108,27 @@ export class CanvasComponent implements OnInit, OnDestroy {
       var m2 = distance(e, c2);
       var m3 = distance(e, c3);
       var m4 = distance(e, c4);
+
       switch (Math.min(m1, m2, m3, m4)) {
+        case m2:
+          w = w + (e.x - c2.x);
+          rect1.attr('width', w + 12);
+          rect2.attr('width', w);
+          break;
         case m3:
           w = w + (e.x - c3.x);
           h = h + (e.y - c3.y);
           rect1.attr('width', w + 12).attr('height', h + 12);
           rect2.attr('width', w).attr('height', h);
-          g.select('text').text(`Perimeter: ${w * 2 + h * 2}`);
-          service.updateFigure({ ...d.subject, width: w, height: h });
+          break;
+        case m4:
+          h = h + (e.y - c4.y);
+          rect1.attr('height', h + 12);
+          rect2.attr('height', h);
           break;
       }
+      g.select('text').text(`Perimeter: ${w * 2 + h * 2}`);
+      service.updateFigure({ ...d.subject, width: w, height: h });
     });
     return [dragGroup, resize];
   }
@@ -107,6 +149,7 @@ export class CanvasComponent implements OnInit, OnDestroy {
           .attr('y', (d) => d.y)
           .attr('transform', (d) => `translate(${d.x},${d.y})`);
         g.append('rect')
+          .attr('class', 'rect1')
           .attr('width', (d) => d.width + 12)
           .attr('height', (d) => d.height + 12)
           .attr('fill', '#999')
@@ -118,10 +161,13 @@ export class CanvasComponent implements OnInit, OnDestroy {
           .attr('height', (d) => d.height)
           .attr('x', 6)
           .attr('y', 6)
+          .attr('rx', 6)
+          .attr('ry', 6)
           .attr('fill', (d) => d.color)
           .call(drag);
         g.append('text')
           .attr('y', -5)
+          .attr('fill', (d) => d.color)
           .text((d) => `Perimeter: ${d.width * 2 + d.height * 2}`);
         return g;
       });
